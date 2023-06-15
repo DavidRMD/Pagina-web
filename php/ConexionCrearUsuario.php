4c6f6048b11b6fb9b0593740ffcbfb96f0eda5fe
David Rodriguez <deinus.rodriguez02@hotmail.com>
2023-06-14 18:17:13 -0600

    Edicion del archivo de conexionCrearUsuario

--- php/ConexionCrearUsuario.php
+++ php/ConexionCrearUsuario.php
@@ -1,29 +1,31 @@
 <?php
+
     include("conexion.php");
 
 
-    $nickname= $_POST("nickname");
-    $nombre= $_POST("nombre");
-    $apellido= $_POST("apellidos");
-    $edad= $_POST("edad");
-    $descripcion= $_POST("descripcion");
-    $email= $_POST("correo");
-    $password= $_POST("contraseña");
+    $nickname   = $_POST["nickname"];
+    $nombre     = $_POST["nombre"];
+    $apellidos  = $_POST["apellidos"];
+    $edad       = $_POST["edad"];
+    $descripcion= $_POST["descripcion"];
+    $email      = $_POST["correo"];
+    $password   = $_POST["contraseña"];
 
-    $passwordHash= password_hash(($password, PASSWORD_BCRYPT));
-    $fotoPerfil= "img/$nickname/perfil.jpg";
+
+    $passwordHash   = password_hash($password, PASSWORD_BCRYPT);
+    $fotoPerfil     = "img/$nickname/perfil.jpg";
 
     //Evaluamos si existe el nickname ingresado
-    $consultaId= "SELECT Nickname
+    $consultaId= "SELECT NickName
                 FROM persona
-                WHERE Nickname= '$nickname'";
-
-    $consultaId= mysqli_query($conexion, $consultaId);  //Devuelve un objeto con el resultado, dalse is hay error, true si se ejecuta
-    $consultald- mysqli_fetch_array ($consultaId): // devuelve un array o NULL
+                WHERE NickName= '$nickname' ";
+    $consultaId = mysqli_query($conexion, $consultaId);  //Devuelve un objeto con el resultado, dalse is hay error, true si se ejecuta
+    $consultaId = mysqli_fetch_array($consultaId); // devuelve un array o NULL
 
     if(!$consultaId){//Si la consulta está vacia entonces significa que no existe el nickname, y creamos el nuevo usuario
+
          $sql="INSERT INTO persona VALUES ( '$nickname', '$nombre', '$apellidos', '$edad', '$descripcion', '$fotoPerfil', '$email', '$passwordHash')";
-    }
+    
         if(mysqli_query($conexion, $sql)){
 
             mkdir("../img/$nickname");
@@ -33,13 +35,14 @@
             echo"<br> <a href='../index.html'>Iniciar Sesion</a></div>";
         }
         else{
-            echo"Error" . $sql . "<br>" - mysqli_error($conexion);
+            echo "Error: " . $sql . "<br>" . mysqli_error($conexion);
         }
+    }
     else{
-        exho"El Nickname ya existe";
+        echo"El Nickname ya existe";
         echo"<a href='index.html'>Intentalo de nuevo.</a></div>";
     }
 
-    mysql_close($conexion);
+    mysqli_close($conexion);
 
 ?>
